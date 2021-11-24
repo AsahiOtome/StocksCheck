dfb9ad2e55ce4a93a5ac039a5bfc425fb62c2d8d
69EE7549EF4DB5D4602E8A8720A52748 <69EE7549EF4DB5D4602E8A8720A52748@i-search.com.cn>
2021-11-24 15:05:46 +0800

    2021-11-24 15:05

--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import json
 import random
 import time
 import requests
+import requests.cookies
 from config import global_config
 import os
 import pickle
@@ -41,7 +42,7 @@ class SpiderSession(object):
     用于对session进行初始化，并提供cookies的存储与调用功能
     """
     def __init__(self):
-        self._cookies_name = './cookies/'
+        self._cookies_path = './cookies/' + global_config.get('settings', 'project_name') + '.cookies'
         self._accept = global_config.get('connect_config', 'accept')
         self._connection = global_config.get('connect_config', 'connection')
         self._user_agent = get_random_users()
@@ -53,7 +54,7 @@ class SpiderSession(object):
         session.headers = self.get_headers()
         return session
 
-    def get_headers(self):
+    def get_headers(self) -> dict:
         headers = {
             'Accept': self._accept,
             'User - Agent': self._user_agent,
@@ -61,14 +62,52 @@ class SpiderSession(object):
         }
         return headers
 
-    def get_user_agent(self):
+    def get_user_agent(self) -> str:
         return self._user_agent
 
-    def get_session(self):
+    def get_session(self) -> requests.sessions.Session:
+        """
+        获取当前session信息
+        :return:
+        """
         return self.session
 
+    def get_cookies(self) -> requests.cookies.RequestsCookieJar:
+        """
+        获取当前session中的cookies信息
+        :return:
+        """
+        return self.get_session().cookies
+
+    def set_cookies(self, cookies: requests.cookies.RequestsCookieJar):
+        """
+        更新session中的cookies内容
+        :param cookies: 读入的cookies文件信息
+        :return:
+        """
+        self.session.cookies.update(cookies)
+
     def save_cookies_to_local(self):
-        if not os.path.exists(self._cookies_save_path):
-            os.makedirs(self._cookies_save_path)
+        """
+        将当前cookies信息保存至本地目录
+        :return:
+        """
+        """
+        pickle.dump() 函数能一个接着一个地将几个对象转储到同一个文件。随后调用 pickle.load() 来以同样的顺序检索这些对象
+        """
+        directory = os.path.join(self._cookies_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory, exist_ok=False)
+        with open(self._cookies_path, 'wb') as fb:
+            pickle.dump(self.get_cookies(), fb)
 
-
+    def load_cookies_from_local(self):
+        """
+        从本地文件中读取旧有cookies信息
+        :return:
+        """
+        if not os.path.exists(self._cookies_path):
+            return False
+        with open(self._cookies_path, 'rb') as fb:
+            cookies = pickle.load(fb)
+        self.set_cookies(cookies)
